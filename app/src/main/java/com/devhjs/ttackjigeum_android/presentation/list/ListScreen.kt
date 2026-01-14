4d6be0382501dd86a9de0c8208ed17595c552857
Jieun Lee <geniee1750@gmail.com>
2026-01-14 15:47:14 +0900

    feat: 상품 목록 화면용 ProductCardItem 및 Badge 컴포넌트 구현 (#1)
    - 상품명, 목표가, 현재가 정보를 포함한 ProductCardItem 컴포넌트 추가
    - 최저가 달성, 가격 하락 등 상태를 표시하는 ProductBadge 컴포넌트 구현
    - ListScreen에 신규 구현된 카드 아이템 적용 및 레이아웃 수정
    - 상품 이미지 부재 시 표시할 ic_noimage 등 신규 아이콘 리소스 추가

--- app/src/main/java/com/devhjs/ttackjigeum_android/presentation/list/ListScreen.kt
+++ app/src/main/java/com/devhjs/ttackjigeum_android/presentation/list/ListScreen.kt
@@ -3,20 +3,21 @@ package com.devhjs.ttackjigeum_android.presentation.list
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.devhjs.ttackjigeum_android.presentation.component.BadgeType
+import com.devhjs.ttackjigeum_android.presentation.component.ProductCardItem
 
 @Composable
 fun ListScreen(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
-            .padding(horizontal = 16.dp)
             .fillMaxSize()
     ) {
-        // TODO: 컨텐츠 추가
+        ProductCardItem(badgeType = BadgeType.LOWEST_PRICE)
+        ProductCardItem(badgeType = BadgeType.PRICE_DROP)
+        ProductCardItem(badgeType = BadgeType.NO_CHANGE)
     }
 }
 
