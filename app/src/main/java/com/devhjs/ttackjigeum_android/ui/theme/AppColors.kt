565de7236d99bd1d3f7546ee8088a98b1e3875d4
japygo <hyunnjinn@gmail.com>
2026-01-14 15:02:19 +0900

    refactor: AppColors를 object로 변환 및 코드 스타일 개선
    - 기존 단일 변수 선언 방식을 object AppColors 내부로 묶음
    - 일관된 코드 스타일로 변수명 정렬 및 개선
    - Color 선언 가독성을 높이고 유지보수 용이성 확보

--- app/src/main/java/com/devhjs/ttackjigeum_android/ui/theme/AppColors.kt
+++ app/src/main/java/com/devhjs/ttackjigeum_android/ui/theme/AppColors.kt
@@ -2,19 +2,21 @@ package com.devhjs.ttackjigeum_android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Primary = Color(0xFF03C75A)
-val Secondary = Color(0xFFEBFBEF)
+object AppColors {
+    val Primary = Color(0xFF03C75A)
+    val Secondary = Color(0xFFEBFBEF)
 
-val AppBackgorund = Color(0xFFEDEEF2)
-val White  = Color(0xFFFEFFFF)
-val Black  = Color(0xFF0D1629)
+    val AppBackground = Color(0xFFEDEEF2)
+    val White = Color(0xFFFEFFFF)
+    val Black = Color(0xFF0D1629)
 
-val TextGray1  = Color(0xFF53585E)
-val TextGray2  = Color(0xFFADB3BC)
+    val TextGray1 = Color(0xFF53585E)
+    val TextGray2 = Color(0xFFADB3BC)
 
 
-val IconGray1  = Color(0xFF64748B)
-val IconGray2  = Color(0xFFF5F6F7)
+    val IconGray1 = Color(0xFF64748B)
+    val IconGray2 = Color(0xFFF5F6F7)
 
-val Warning1 = Color(0xFFCD4243)
-val Warning2 = Color(0xFFFBECEC)
+    val Warning1 = Color(0xFFCD4243)
+    val Warning2 = Color(0xFFFBECEC)
+}
