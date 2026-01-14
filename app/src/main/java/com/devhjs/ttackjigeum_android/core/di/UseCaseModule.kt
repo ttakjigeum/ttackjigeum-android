2659bc0c07854a3707702559b70bb9463e81f24f
wantfree8937 <qkrtmdduq112@naver.com>
2026-01-14 18:38:38 +0900

    feat: 리뷰 반영 (#39)

--- app/src/main/java/com/devhjs/ttackjigeum_android/core/di/UseCaseModule.kt
+++ app/src/main/java/com/devhjs/ttackjigeum_android/core/di/UseCaseModule.kt
@@ -5,6 +5,6 @@ import com.devhjs.ttackjigeum_android.domain.usecase.GetProductUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
-    factory { GetProductUseCase(get()) }
-    factory { GetPriceHistoryUseCase(get()) }
+    single { GetProductUseCase(get()) }
+    single { GetPriceHistoryUseCase(get()) }
 }
--- app/src/main/java/com/devhjs/ttackjigeum_android/presentation/component/PriceInfoCard.kt
+++ app/src/main/java/com/devhjs/ttackjigeum_android/presentation/component/PriceInfoCard.kt
@@ -10,9 +10,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.BarChart
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
