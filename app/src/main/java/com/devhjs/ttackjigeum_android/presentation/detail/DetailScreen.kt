e4bc366a44a9216a148ff85d9d93d01f51a9f171
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-01-21 16:10:27 +0900

    feat: 상세 화면 로딩 인디케이터 추가 및 목표 가격 입력 로직 개선 (#100)

--- app/src/main/java/com/devhjs/ttackjigeum_android/presentation/detail/DetailScreen.kt
+++ app/src/main/java/com/devhjs/ttackjigeum_android/presentation/detail/DetailScreen.kt
@@ -1,6 +1,7 @@
 package com.devhjs.ttackjigeum_android.presentation.detail
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,6 +19,7 @@ import com.devhjs.ttackjigeum_android.presentation.component.DetailBottomBar
 import com.devhjs.ttackjigeum_android.presentation.component.DetailProductCard
 import com.devhjs.ttackjigeum_android.presentation.component.DetailTopAppBar
 import com.devhjs.ttackjigeum_android.presentation.component.HistoryGraph
+import com.devhjs.ttackjigeum_android.presentation.component.LoadingCircleIndicator
 import com.devhjs.ttackjigeum_android.presentation.component.PriceInfoCard
 import com.devhjs.ttackjigeum_android.presentation.component.TargetPriceSlider
 import com.devhjs.ttackjigeum_android.ui.theme.AppColors
@@ -41,54 +43,64 @@ fun DetailScreen(
         },
         containerColor = AppColors.White
     ) { paddingValues ->
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-                .verticalScroll(rememberScrollState())
-                .background(AppColors.White)
         ) {
-            state.product?.let { product ->
-                DetailProductCard(
-                    name = product.name,
-                    imageUrl = product.imageUrl
-                )
+            if (state.isLoading) {
+                LoadingCircleIndicator()
+            } else {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(rememberScrollState())
+                        .background(AppColors.White)
+                ) {
+                    state.product?.let { product ->
+                        DetailProductCard(
+                            name = product.name,
+                            imageUrl = product.imageUrl
+                        )
 
-                Text(
-                    text = product.name,
-                    style = AppTextStyles.largeTextBold,
-                    color = AppColors.Black,
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
+                        Text(
+                            text = product.name,
+                            style = AppTextStyles.largeTextBold,
+                            color = AppColors.Black,
+                            modifier = Modifier.padding(horizontal = 16.dp)
+                        )
 
-                Spacer(modifier = Modifier.height(8.dp))
+                        Spacer(modifier = Modifier.height(8.dp))
 
-                PriceInfoCard(
-                    currentPrice = product.currentPrice,
-                    originalPrice = product.originalPrice.takeIf { it > product.currentPrice }, // 원가 검증
-                    targetPrice = product.targetPrice,
-                    lowestPrice = product.lowestPrice,
-                    averagePrice = product.averagePrice
-                )
+                        PriceInfoCard(
+                            currentPrice = product.currentPrice,
+                            originalPrice = product.originalPrice.takeIf { it > product.currentPrice }, // 원가 검증
+                            targetPrice = product.targetPrice,
+                            lowestPrice = product.lowestPrice,
+                            averagePrice = product.averagePrice
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(24.dp))
+
+                    HistoryGraph(
+                        histories = state.priceHistories
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    state.product?.let { product ->
+                        TargetPriceSlider(
+                            targetPrice = product.targetPrice,
+                            currentPrice = product.currentPrice,
+                            onTargetPriceChange = { onAction(DetailAction.OnTargetPriceChange(it)) }
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(100.dp)) // 바텀 바를 위한 추가 여백 공간
+                }
             }
 
-            Spacer(modifier = Modifier.height(24.dp))
-
-            HistoryGraph(
-                histories = state.priceHistories
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            state.product?.let { product ->
-                TargetPriceSlider(
-                    targetPrice = product.targetPrice,
-                    currentPrice = product.currentPrice,
-                    onTargetPriceChange = { onAction(DetailAction.OnTargetPriceChange(it)) }
-                )
-            }
-
-            Spacer(modifier = Modifier.height(100.dp)) // 바텀 바를 위한 추가 여백 공간
         }
     }
 }
@@ -98,6 +110,7 @@ fun DetailScreen(
 fun DetailScreenPreview() {
     DetailScreen(
         state = DetailState(
+            isLoading = true,
             product = com.devhjs.ttackjigeum_android.domain.model.Product(
                 id = 1,
                 name = "Sony WH-1000XM5",
